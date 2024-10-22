879a5137f5c6f351c3e730afcbd8885f55b9b284
Zhang Ning <832666+zhangn1985@users.noreply.github.com>
2024-10-22 09:25:20 +0800

    uwe5622: fix build with 6.12
    Signed-off-by: Zhang Ning <832666+zhangn1985@users.noreply.github.com>

--- drivers/net/wireless/uwe5622/unisocwifi/wl_core.c
+++ drivers/net/wireless/uwe5622/unisocwifi/wl_core.c
@@ -704,7 +704,7 @@ err:
 	return ret;
 }
 
-static int sprdwl_remove(struct platform_device *pdev)
+static void sprdwl_remove(struct platform_device *pdev)
 {
 	struct sprdwl_intf *intf = platform_get_drvdata(pdev);
 	struct sprdwl_priv *priv = intf->priv;
@@ -724,7 +724,6 @@ static int sprdwl_remove(struct platform_device *pdev)
 	stop_marlin(MARLIN_WIFI);
 	wl_info("%s\n", __func__);
 
-	return 0;
 }
 
 static const struct of_device_id sprdwl_of_match[] = {
